20f1934fdadac6dcdb1759516044e3f2936964e7
emobozzman <emobozzman@gmail.com>
2020-10-16 21:49:13 +0300

    aerocommit

--- src/framework/consts.py
+++ src/framework/consts.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 SERVER_RUNNING_BANNER = """
 +----------------------------------------+
 |             SERVER WORKS!              |
@@ -7,3 +9,13 @@ Visit http://{host}:{port}
 
 ..........................................
 """
+
+_this_file_path = Path(__file__).resolve()
+
+DIR_FRAMEWORK = _this_file_path.parent.resolve()
+
+DIR_SRC = DIR_FRAMEWORK.parent.resolve()
+
+DIR_REPO = DIR_SRC.parent.resolve()
+
+DIR_STATIC = (Path(__file__).parent.parent / "static").resolve()
